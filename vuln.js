4db8f14911040808548c8dd82b7c65159508dcc1
Param Jani <115683209+ParamJani21@users.noreply.github.com>
2026-03-14 19:10:03 +0530

    Add files via upload

--- vuln.js
+++ vuln.js
@@ -1,10 +1,27 @@
-const express = require('express');
-const app = express();
+const express = require("express")
+const { exec } = require("child_process")
 
-app.get('/', (req, res) => {
-    // Intentional vulnerability (Eval)
-    eval(req.query.code);
-    res.send('Hello World!');
-});
+const app = express()
 
-app.listen(3000);
+// Hardcoded secret
+const API_KEY = "super-secret-api-key"
+
+app.get("/run", (req, res) => {
+    const cmd = req.query.cmd
+
+    // Command injection
+    exec(cmd, (err, stdout) => {
+        res.send(stdout)
+    })
+})
+
+app.get("/eval", (req, res) => {
+    const input = req.query.code
+
+    // Eval injection
+    eval(input)
+
+    res.send("executed")
+})
+
+app.listen(3000)
