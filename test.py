9354f74132d2fd3949c18bb7cf03624995c69183
Param Jani <115683209+ParamJani21@users.noreply.github.com>
2026-03-23 20:57:01 +0530

    Fix typo in warning comment in test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# vulnerable_code.py - DO NOT USaaaaaaaaasssssssssssssaaaaaaaaaaaE IN PRODUCTION!
+# vulnerable_code.py - DO NOT USaaaaaaaaasssssssssssssaaaaaaaaahhaaE IN PRODUCTION!
 #admin:password 
 import os
 import subprocess
